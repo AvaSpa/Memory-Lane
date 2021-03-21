375a966ec95d3eb5f7e930bd64c03ba9530b444a
Adrian Cigher <adrian.cigher@gmail.com>
2021-03-21 14:14:31 +0200

    Implemented level asset creation tool

---
+++ Memory Lane/Assets/Scripts/ScriptableObjects/Level.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(fileName = "Level", menuName = "Levels/New Level")]
+public class Level : ScriptableObject
+{
+    public List<Vector2> Tiles;
+}
---
+++ Memory Lane/Assets/Scripts/ScriptableObjects/MakeLevelList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.ScriptableObjects
+{
+    public class MakeLevelList
+    {
+        [MenuItem("Assets/Create/LevelList", false, 0)]
+        public static void CreateLevelList()
+        {
+            var level1 = ScriptableObject.CreateInstance<Level>();
+            level1.Tiles = new List<Vector2> { new Vector2 { x = 4, y = 7 }, new Vector2 { x = 4, y = 6 } };
+            AssetDatabase.CreateAsset(level1, "Assets/Levels/Level1.asset");
+            AssetDatabase.SaveAssets();
+
+            var list = ScriptableObject.CreateInstance<LevelList>();
+            list.Levels = new List<Level> { level1 };
+            AssetDatabase.CreateAsset(list, "Assets/Levels/LevelList.asset");
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
