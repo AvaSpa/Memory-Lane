8f63180b45d696fe4863f715f60b771cb2514d54
Adrian Cigher <adrian.cigher@gmail.com>
2021-03-21 15:01:00 +0200

    Created level list asset
    Menu -> Assets -> Create -> Level List

--- Memory Lane/Assets/Scripts/ScriptableObjects/MakeLevelList.cs
+++ Memory Lane/Assets/Scripts/ScriptableObjects/MakeLevelList.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,16 +8,48 @@ namespace Assets.Scripts.ScriptableObjects
 {
     public class MakeLevelList
     {
-        [MenuItem("Assets/Create/LevelList", false, 0)]
+        [MenuItem("Assets/Create/Level List", false, 0)]
         public static void CreateLevelList()
         {
-            var level1 = ScriptableObject.CreateInstance<Level>();
-            level1.Tiles = new List<Vector2> { new Vector2 { x = 4, y = 7 }, new Vector2 { x = 4, y = 6 } };
-            AssetDatabase.CreateAsset(level1, "Assets/Levels/Level1.asset");
+            AssetDatabase.DeleteAsset("Assets/Levels");
+            AssetDatabase.CreateFolder("Assets", "Levels");
             AssetDatabase.SaveAssets();
 
             var list = ScriptableObject.CreateInstance<LevelList>();
-            list.Levels = new List<Level> { level1 };
+            list.Levels = new List<Level>();
+
+            var lines = File.ReadAllLines(@"..\Data\Levels.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var level = ScriptableObject.CreateInstance<Level>();
+                level.Tiles = new List<Vector2>();
+
+                var line = lines[i];
+                try
+                {
+                    var tileSplit = line.Split();
+                    foreach (var tile in tileSplit)
+                    {
+                        var pointSplit = tile.Split(',');
+                        var x = int.Parse(pointSplit[0]);
+                        var y = int.Parse(pointSplit[1]);
+                        var vector = new Vector2(x, y);
+
+                        level.Tiles.Add(vector);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                    Debug.Log(line);
+                }
+
+                AssetDatabase.CreateAsset(level, $"Assets/Levels/Level{i + 1}.asset");
+                AssetDatabase.SaveAssets();
+
+                list.Levels.Add(level);
+            }
+
             AssetDatabase.CreateAsset(list, "Assets/Levels/LevelList.asset");
             AssetDatabase.SaveAssets();
         }
