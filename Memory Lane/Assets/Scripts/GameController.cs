96c8d2a90a0ac03bf796f4dfa63e3bc773971b42
Adrian Cigher <adrian.cigher@gmail.com>
2021-03-22 19:40:07 +0200

    The camera moves from full view to play view at start of level

--- Memory Lane/Assets/Scripts/GameController.cs
+++ Memory Lane/Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
@@ -9,12 +10,41 @@ public class GameController : MonoBehaviour
     public GameObject ResetButton;
     public CountDown CountDown;
     public int CurrentLevel;
+    public Transform Camera;
+    public int CameraTranslationSpeed;
+    public int CameraRotationSpeed;
 
     private const string LevelKey = "Level";
 
     private void Awake()
     {
         CurrentLevel = PlayerPrefs.GetInt(LevelKey, 1);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(MoveCameraToPlayPosition(CameraTranslationSpeed));
+        StartCoroutine(RotateCameraToPlayPosition(CameraRotationSpeed));
+    }
+
+    private IEnumerator MoveCameraToPlayPosition(float speed = 1)
+    {
+        var destination = new Vector3(20, 40, -30);
+        while (Camera.position != destination)
+        {
+            Camera.position = Vector3.MoveTowards(Camera.position, destination, speed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator RotateCameraToPlayPosition(float speed = 1)
+    {
+        var destination = new Vector3(75, 0, -20);
+        while (Camera.rotation.eulerAngles != destination)
+        {
+            Camera.rotation = Quaternion.RotateTowards(Camera.rotation, Quaternion.Euler(destination), speed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     public void EndGame(bool fail)
