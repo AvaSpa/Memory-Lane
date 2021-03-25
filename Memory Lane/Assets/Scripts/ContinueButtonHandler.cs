5f586ea5f9ee338bd7e17c08a5dabfede03b4562
Adrian Cigher <adrian.cigher@gmail.com>
2021-03-25 20:38:31 +0200

    Implemented level select

--- Memory Lane/Assets/Scripts/ContinueButtonHandler.cs
+++ Memory Lane/Assets/Scripts/ContinueButtonHandler.cs
@@ -4,13 +4,8 @@ public class ContinueButtonHandler : MonoBehaviour
 {
     public SceneChanger SceneChanger;
 
-    private const string CurrentLevelKey = "CurrentLevel";
-    private const string MaxLevelKey = "MaxLevel";
-
     public void Continue()
     {
         SceneChanger.FadeToScene(Assets.Scripts.Enums.SceneIdentity.Main);
     }
-
-    //TODO: get selected level from list and set that as current level before fading to main
 }
---
+++ Memory Lane/Assets/Scripts/MenuController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public LevelList Levels;
+    public RectTransform UiList;
+    public GameObject UiListItem;
+    public SceneChanger SceneChanger;
+
+    private const string CurrentLevelKey = "CurrentLevel";
+    private const string MaxLevelKey = "MaxLevel";
+
+    private void Start()
+    {
+        var maxReachedLevel = PlayerPrefs.GetInt(MaxLevelKey, 1);
+        var currentLevel = PlayerPrefs.GetInt(CurrentLevelKey, 1);
+        if (maxReachedLevel < currentLevel)
+        {
+            maxReachedLevel = currentLevel;
+            PlayerPrefs.SetInt(MaxLevelKey, maxReachedLevel);
+        }
+
+        for (var i = 0; i < maxReachedLevel; i++)
+        {
+            var listItem = GameObject.Instantiate(UiListItem, UiList);
+            var itemText = listItem.GetComponentInChildren<Text>();
+            itemText.text += i + 1;
+
+            var button = listItem.GetComponent<Button>();
+            button.onClick.AddListener(() =>
+            {
+                PlayerPrefs.SetInt(CurrentLevelKey, i);
+                SceneChanger.FadeToScene(Assets.Scripts.Enums.SceneIdentity.Main);
+            });
+        }
+    }
+}
