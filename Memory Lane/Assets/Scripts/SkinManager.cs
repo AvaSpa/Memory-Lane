511ca6310255c82730bfcc6c1e5072dee34cea9a
Adrian Cigher <adrian.cigher@gmail.com>
2022-02-08 22:04:13 +0200

    Implemented skin unlock mechanism

--- Memory Lane/Assets/Scripts/SkinManager.cs
+++ Memory Lane/Assets/Scripts/SkinManager.cs
@@ -11,29 +11,44 @@ public class SkinManager : MonoBehaviour
     private List<SkinButtonHandler> _skinButtons = new List<SkinButtonHandler>();
 
     private const string CurrentSkinKey = "CurrentSkin";
+    private const string MaxLevelKey = "MaxLevel";
 
     private void Start()
     {
+        var currentSkin = PlayerPrefs.GetInt(CurrentSkinKey, 0);
+        var maxLevel = PlayerPrefs.GetInt(MaxLevelKey, 1);
+
+        var maxSkin = (maxLevel - 10) / 5 + 1;
+        if (maxSkin < currentSkin)
+            maxSkin = currentSkin;
+        if (maxSkin >= Skins.Skins.Count)
+            maxSkin = Skins.Skins.Count - 1;
+
+        if (maxSkin == 0)
+            AddButton(0);
+        else
+            for (var i = 0; i <= maxSkin; i++)
+                AddButton(i);
+
+        MarkSelectedSkin(currentSkin);
+    }
+
+    private void AddButton(int i)
+    {
         const int buttonOffset = 20;
 
-        for (var i = 0; i < Skins.Skins.Count; i++)
-        {
-            var listItem = GameObject.Instantiate(SkinButtonPrefab, ButtonContainer, false);
-            var position = listItem.transform.position;
+        var listItem = GameObject.Instantiate(SkinButtonPrefab, ButtonContainer, false);
+        var position = listItem.transform.position;
 
-            listItem.transform.position = new Vector3(position.x, position.y + buttonOffset - i * 15, position.z);
+        listItem.transform.position = new Vector3(position.x, position.y + buttonOffset - i * 15, position.z);
 
-            var skinButtonHandler = listItem.GetComponent<SkinButtonHandler>();
-            skinButtonHandler.SkinId = i;
-            skinButtonHandler.Skins = Skins;
-            skinButtonHandler.SkinManager = this;
-            _skinButtons.Add(skinButtonHandler);
+        var skinButtonHandler = listItem.GetComponent<SkinButtonHandler>();
+        skinButtonHandler.SkinId = i;
+        skinButtonHandler.Skins = Skins;
+        skinButtonHandler.SkinManager = this;
+        _skinButtons.Add(skinButtonHandler);
 
-            SetVisibility(listItem, i);
-        }
-
-        var currentSkinId = PlayerPrefs.GetInt(CurrentSkinKey, 0);
-        MarkSelectedSkin(currentSkinId);
+        SetVisibility(listItem, i);
     }
 
     private void SetVisibility(GameObject listItem, int index)
