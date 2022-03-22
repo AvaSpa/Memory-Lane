8aabf2d100d11b8a41849c32fbcb338f61a203f8
Adrian Cigher <adrian.cigher@gmail.com>
2022-03-22 21:00:46 +0200

    Updated Advertisment package; changed privacy policy url

--- Memory Lane/Assets/Scripts/PrivacyPolicyOpener.cs
+++ Memory Lane/Assets/Scripts/PrivacyPolicyOpener.cs
@@ -4,6 +4,6 @@ public class PrivacyPolicyOpener : MonoBehaviour
 {
     public void OpenPrivacyPolicy()
     {
-        Application.OpenURL("https://docs.google.com/document/d/1M4Gd8gYxvBLJelFrnDduq6yh1UXJiD5OB8IIm5BzGjQ/edit?usp=sharing");
+        Application.OpenURL("https://avaspa.azurewebsites.net/privacypolicy.html");
     }
 }
