d0a53c35e698c2cc3f475651851a0454580dc8c1
Adrian Cigher <adrian.cigher@gmail.com>
2022-05-06 17:59:21 +0300

    Showing ads from first 7 levels completed and every 2 levels +
    Fixed ad initialization

--- Memory Lane/Assets/Scripts/AdsController.cs
+++ Memory Lane/Assets/Scripts/AdsController.cs
@@ -6,23 +6,26 @@ public class AdsController : MonoBehaviour
 {
     private const string GameId = "4611537";
     private const string PlacementId = "Interstitial_Android";
-    private const int LevelInterval = 5;
+    private const int LevelInterval = 2;
+    private const int MinimumLevel = 7;
     private const bool TestMode = false;
 
     private int _playedLevelCount;
     private int _maxReachedLevel;
 
 
-    private void Start()
+    private void Awake()
     {
         _maxReachedLevel = PlayerPrefs.GetInt(PlayerPrefsKeys.MaxLevelKey, 1);
 
         Advertisement.Initialize(GameId, TestMode);
+
+        Debug.Log("Initialized ads.");
     }
 
     public void SignalLevelCompleted()
     {
-        if (_maxReachedLevel < 10) return;
+        if (_maxReachedLevel < MinimumLevel) return;
 
         if (_playedLevelCount == 0 && Advertisement.isInitialized)
             Advertisement.Show(PlacementId);
